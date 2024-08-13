5f2393726dfc92fcfe01e81fbef10694917617b6
Mikiii000 <miquelaragones1@gmail.com>
2024-08-13 13:35:41 +0200

    Moviment jugador i hitboxes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,22 +5,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public InputActionAsset actions;
     private Vector2 move;
     [Range(1, 50)]
     public int speed = 1;
+    private Rigidbody rigidbody;
+    public float acceleration = 10;
+    public float deceleration = 10;
+    public float power = 0.96f;
 
     void Start() // Start is called before the first frame update
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
     void Update() // Update is called once per frame
     {
-        gameObject.transform.position += new Vector3(move.x, 0, move.y) * speed * Time.deltaTime;
+        Run();
     }
 
     public void onMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
+    }
+
+    private void Run()
+    {
+        Vector2 targetspeed = move * speed;
+        Vector2 speedDiff = new Vector2(targetspeed.x - rigidbody.velocity.x,
+                                        targetspeed.y - rigidbody.velocity.z);
+        Vector2 accelRate = new Vector2((Mathf.Abs(targetspeed.x) > 0.01f) ? acceleration : deceleration,
+                                        (Mathf.Abs(targetspeed.y) > 0.01f) ? acceleration : deceleration);
+        Vector2 movement = new Vector2(Mathf.Pow(Mathf.Abs(speedDiff.x) * accelRate.x, power) * Mathf.Sign(speedDiff.x),
+                                      Mathf.Pow(Mathf.Abs(speedDiff.y) * accelRate.y, power) * Mathf.Sign(speedDiff.y));
+        rigidbody.AddForce(new Vector3 (movement.x, 0, movement.y));
     }
 }
